4286eb61bc647829e87d8e2b6794cf40db4c0647
Gert Vermeersch <gert@Bandit-2.local>
2014-08-13 22:44:17 +0200

    initial commit

---
+++ DomoticaMasterNode.ino
@@ -0,0 +1,34 @@
+#include <NRF905.h>
+#include <domotica.h>
+#include <SPI.h>
+
+Domotica controller;
+//NRF905 nrf;
+char testmsg[MSG_LEN] = {"012345678901234567890123456"};
+long sent = 0;
+long received = 0;
+
+void setup() {
+  Serial.begin(9600);
+  //nrf = NRF905();
+  controller = Domotica();
+  controller.setDebug(true);
+  controller.init(0);  
+  
+  
+}
+
+void loop() {
+  bool res = controller.sendToNode(2,testmsg);
+  sent++;
+  //delay(10);
+  if(res) received++;
+  Serial.print("Sent:\t");
+  Serial.print(sent);
+  Serial.print("\tReceived:\t");
+  Serial.print(received);
+  float perc = (received * 100) / sent;
+  Serial.print("\t%:\t");
+  Serial.println(perc);
+  delay(2000);
+}
